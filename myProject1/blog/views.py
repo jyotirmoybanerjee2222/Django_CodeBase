298a06d9ccedb42306c63f822489caa72da34dd9
Jyotirmoy Banerjee <jyotirmoy.banerjee@tangenttechsolutions.com>
2026-02-14 18:18:13 +0530

    first commit

---
+++ myProject1/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def post_details(request,post_id):
+    return HttpResponse(f"<h1>Show Blog of Post {post_id}</h1>")
+
+def user_profile(request,username):
+    return HttpResponse(f"<h1>Profile of User:{username}</h1>")
+
+def article_by_year(request,year):
+    return HttpResponse(f"<h1>Articles from the year {year}</h1>")
+
+# def article_details(request,year,month):
+#     return HttpResponse(f"<h1>Articles from {year} - {month}</h1>")
+
+def article_details(request,**kwargs):
+    return HttpResponse(f"<h1>Data: {kwargs}</h1>")
